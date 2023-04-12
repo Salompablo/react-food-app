2bf294d27097a726aed88fecb683084bd9eb3afa
Salompablo <pablosalompita@gmail.com>
2023-04-12 15:17:43 +0200

    Add Modal component using React Portal

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,11 +1,11 @@
 import Input from '../../UI/Input';
 import classes from './MealItemForm.module.css';
 
-export default function MealItemForm() {
+export default function MealItemForm(props) {
   return (
     <form className={classes.form}>
         <Input label="Amount" input={{
-            id: 'amount',
+            id: 'amount_' + props.id,
             type: 'number',
             min: '1',
             max: '5',
