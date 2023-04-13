f536d6177920b7429ce2e5fd70bce63f8661d102
Salompablo <pablosalompita@gmail.com>
2023-04-13 17:57:03 +0200

    Add modal click functions and general context
    These 'click functions' are the usual functions for the "close" button for the modal or on clicking outside the modal.
    
    Created the first part of the logic for "data" entry using useContext. Created the context and provided to the entire app.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
-import { Fragment } from "react";
+import { useState } from "react";
 import Cart from "./components/Cart/Cart";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
+import CartProvider from "./store/CartProvider";
 
 function App() {
+
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  function showCartHandler() {
+    setCartIsShown(true);
+  };
+
+  function hideCartHandler() {
+    setCartIsShown(false);
+  };
+
   return (
-    <Fragment>
-    <Cart />
-      <Header />
+    <CartProvider>
+    {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} onHideCart={hideCartHandler}  />
       <main>
         <Meals />
       </main>
-    </Fragment>
+    </CartProvider>
   );
 }
 
